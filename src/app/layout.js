4492e5c66086c68edb0d6e685a4c74e722cc0b78
Aleksandra Bozhinoska <aleksandra.bozhinoska@sonarsource.com>
2026-06-03 16:11:05 +0200

    Update value

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-const anotherVariable = 4;
+const anotherVariable = 5;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
