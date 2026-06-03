2e90233174a9e6192a0354a1383d8d94c2af8696
Aleksandra Bozhinoska <aleksandra.bozhinoska@sonarsource.com>
2026-06-03 14:58:13 +0200

    Remove unused variable

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-const anotherVariable = 3;
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
